f2187b225dc36a47b29a28d2df4e4f55f9474842
soeatu <soeatsu@gmail.com>
2023-07-18 00:10:29 +0900

    SSG midway

---
+++ components/Layout.tsx
@@ -0,0 +1,58 @@
+import styles from "./layout.module.css";
+import React, { useState, useEffect, ReactNode } from "react";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "Blogs";
+export const siteTitle = "Next.js Blog";
+
+type LayoutProps = {
+  children: ReactNode;
+  home?: boolean;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children, home }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
+              alt={name}
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <img
+                src="/images/profile.png"
+                alt=""
+                className={`${styles.headerImage} ${utilStyles.borderCircle}`}
+              />
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link legacyBehavior href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div>
+          <Link href="/">← ホームへ戻る</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,125 +1,82 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Link from "next/link";
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Layout, { siteTitle } from "../components/Layout";
+import utilStyles from "../styles/utils.module.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { getSortedPostsData } from "../lib/posts";
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Next.js Blog</title>
-      </Head>
-      <main
-        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-      >
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-            GGGGGG started by editing&nbsp;
-            <code className="font-mono font-bold">pages/index.tsx</code>
-          </p>
-          <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            <a
-              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className="dark:invert"
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+import Link from "next/link";
+//import Date from "../components/date";
 
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-          <Image
-            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Docs{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <Link
-            href="/posts/firestpostt"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Learn{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </Link>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Templates{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={`mb-3 text-2xl font-semibold`}>
-              Deploy{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+interface PostData {
+  id: string;
+  [key: string]: any;
 }
+
+//SSGの場合
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData(); //mdファイルのid,titel,date,thumbnailを取得
+  return {
+    props: {
+      allPostsData, //これをHomeに渡す
+    },
+  };
+}
+
+//SSRの場合(contextにはrequestされたときのパラメータが入る)
+// export async function getServerSideProps(context) {
+//   return {
+//     props: {
+//       //コンポーネントに渡すためのprops
+//     },
+//   };
+// }
+
+const Home: React.FC<PostData> = ({ allPostsData }) => {
+  return (
+    <Layout home>
+      <Head>
+        <title>siteTitle</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavascriptです
+        </p>
+        {/* <Link href="/posts/first-post">最初の投稿はこちら</Link>  あとで外す*/}
+      </section>
+
+      {/* gridで表示したい */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>📝エンジニアのブログ</h2>
+
+        <div className={`${styles.grid}`}>
+          {/* 1つ１つのブログをdivで生成してgrid適用させる */}
+          {allPostsData.map(({ id, date, title, thumbnail }) => (
+            <article key={id}>
+              <Link href={`/posts/${id}`}>
+                <img
+                  src={`${thumbnail}`}
+                  className={`${styles.thumbnailImage}`}
+                />
+              </Link>
+              <Link href={`/posts/${id}`}>
+                <a className={utilStyles.boldText}>{title}</a>
+              </Link>
+              {/* {title} */}
+              <br />
+              {/* {id}
+              <br />
+              {date} */}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </article>
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default Home;
